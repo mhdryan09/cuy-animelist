85826d0911e10a56006f9d0e5417c85f0b1fc270
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-10-30 23:07:52 +0700

    Fixing Fit Images

--- src/app/components/AnimeList/index.js
+++ src/app/components/AnimeList/index.js
@@ -4,7 +4,7 @@ import Link from "next/link"
 const AnimeList = ({ id, title, images }) => {
   return (
     <Link href={`/${id}`} className="cursor-pointer">
-      <Image src={images} alt="logo" width={350} height={350}></Image>
+      <Image src={images} alt="logo" width={350} height={350} className="w-full max-h-64 object-cover"></Image>
       <h3 className="font-bold md:text-xl p-4 text-sm">{title}</h3>
     </Link>
   )
