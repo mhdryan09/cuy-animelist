d328805655c8db833a8099a20cc0ecaad233c012
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-10-31 13:35:34 +0700

    Loading Halaman

---
+++ src/app/loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div class="flex justify-center items-center min-h-screen">
+      <div className='loading'>
+      </div>
+    </div>
+  )
+}
+
+export default Loading
