eaa15075e695247286c6895d0cfbd411cda49abd
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-10-31 10:40:09 +0700

    [ENHANCEMENT] Component Reusability

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,19 @@
-import Link from "next/link";
-import AnimeList from "./components/AnimeList";
+import AnimeList from "@/components/AnimeList";
+import Header from "@/components/AnimeList/Header";
 
 const Home = async () => {
   // fetching data
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`);
-  const animes = await response.json();
+  const topAnime = await response.json();
 
   return (
-    <div>
-      <div className="p-4 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Paling Populer</h1>
-        <Link href={"/populer"} className="md:text-xl text-sm underline hover:text-indigo-500 transition-all">Lihat Semua</Link>
-      </div>
-      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
-        {animes.data.map((anime) => {
-          return (
-            <div key={anime.mal_id} className="shadow-xl">
-              <AnimeList id={anime.mal_id} title={anime.title} images={anime.images.webp.image_url} />
-            </div>
-          )
-        })}
-      </div>
-    </div>
+    <>
+    {/* Anime Terpopuler */}
+      <section>
+        <Header title={"Paling Populer"} linkHref="/populer" linkTitle="Lihat Semua" />
+        <AnimeList api={topAnime} />
+      </section>
+    </>
   )
 }
 
