1a3c0533db1467648bb84b4fd563d19c0a444c1e
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-10-31 14:30:36 +0700

    [FIXING] : Decode URI, JSX File

--- catatan/Catatan.md
+++ catatan/Catatan.md
@@ -6,3 +6,9 @@ Folder Structure
     - search      => localhost:3000/search
 
   - component (berguna untuk tampilan UI)
+
+
+# Sesi4
+  - Apabila sebuah file isinya atau mengandung rendering halaman maka kita bisa ubah menjadi ektensi .jsx
+  - jika pada halaman file isinya seperti perhitungan, validasi, dll maka bisa gunakan file ekstensi .js
+  
--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -5,15 +5,17 @@ const Search = async ({ params }) => {
   // Mengambil keyword dari URL parameters
   const keyword = params.keyword;
 
+  const decodedKeyword = decodeURI(keyword); // decode URI untuk mengubah tanda baca ke huruf normal
+
   // Mengambil data anime berdasarkan keyword
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${keyword}`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${decodedKeyword}`);
   // ubah data jadi json
   const searchAnime = await response.json();
 
   return (
     <>
       <section>
-        <Header title={`Hasil Pencarian ${keyword}`} />
+        <Header title={`Hasil Pencarian ${decodedKeyword}...`} />
         <AnimeList api={searchAnime} />
       </section>
     </>
