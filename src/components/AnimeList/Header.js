81be6d95d2fa00a0c0ce087aca2eb698c3d13d17
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-10-31 13:13:48 +0700

    UI Theming

--- src/components/AnimeList/Header.js
+++ src/components/AnimeList/Header.js
@@ -4,12 +4,12 @@ import Link from "next/link";
 const Header = ({ title, linkHref, linkTitle }) => {
   return (
     <div className="p-4 flex justify-between items-center">
-      <h1 className="text-2xl font-bold"> {title} </h1>
+      <h1 className="text-2xl font-bold text-color-primary"> {title} </h1>
       {/* jika ada linkHref dan linkTitle maka tampilkan semua */}
       {linkHref && linkTitle ? (
         <Link
           href={`${linkHref}`}
-          className="md:text-xl text-sm underline hover:text-indigo-500 transition-all"
+          className="md:text-xl text-sm underline text-color-primary hover:text-color-accent transition-all"
         >
           {linkTitle}
         </Link>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,9 +4,9 @@ import InputSearch from './InputSearch'
 
 const Navbar = () => {
   return (
-    <header className='bg-indigo-500'>
-      <div className='flex md:flex-row flex-col gap-2 justify-between p-4'>
-        <Link href="/" className='font-bold text-white text-2xl'>Cuy Anime List</Link>
+    <header className='bg-color-accent'>
+      <div className='flex md:flex-row flex-col gap-2 justify-between md:items-center p-4'>
+        <Link href="/" className='font-bold text-2xl'>Cuy Anime List</Link>
         <InputSearch />
       </div>
     </header>
