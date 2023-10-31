c3f5c5b0a2485fb5fc2fc89542a05efcd480543c
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-10-31 12:30:16 +0700

    [TUGAS] - Search Input dengan Enter

--- src/components/Navbar/InputSearch.js
+++ src/components/Navbar/InputSearch.js
@@ -9,19 +9,22 @@ const InputSearch = () => {
   const router = useRouter(); // Router for routing to different pages
 
   const handleSearch = (e) => {
-    // matikan default submit
-    e.preventDefault();
-    // ambil inputan value
-    const keyword = searchRef.current.value;
-    // redirect halaman search dgn keyword
-    router.push(`/search/${keyword}`);
-    // kosongkan inputan / reset inputan
-    searchRef.current.value = "";
+    // jika tombol enter ditekan atau tombol di klik
+    if (e.key === "Enter" || e.type === "click") {
+      // matikan default submit
+      e.preventDefault();
+      // ambil inputan value
+      const keyword = searchRef.current.value;
+      // redirect halaman search dgn keyword
+      router.push(`/search/${keyword}`);
+      // kosongkan inputan / reset inputan
+      searchRef.current.value = "";
+    }
   }
 
   return (
     <div className="relative">
-      <input ref={searchRef} type="text" placeholder='Cari Anime...' className="w-full p-2 rounded" />
+      <input ref={searchRef} type="text" placeholder='Cari Anime...' className="w-full p-2 rounded" onKeyDown={handleSearch} />
       <button className="absolute top-2 end-3" onClick={handleSearch}>
         <MagnifyingGlass size={24} />
       </button>
